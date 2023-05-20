d9a8a37a8439347bbd48e1f1ba61af700b201668
Wesley Ada <wesley@wesleyada.com>
2023-05-20 11:40:24 -0500

    Adding license

--- cli/main.go
+++ cli/main.go
@@ -1,3 +1,21 @@
+/* Copyright 2023 White Box VPN
+
+This file is part of White Box VPN CLI.
+
+White Box VPN CLI is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+White Box VPN CLI  is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with
+White Box VPN CLI. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package main
 
 import (
@@ -39,7 +57,7 @@ func main() {
 		printHelp()
 	}
 
-	if loginCommand.Parsed() {
+ 	if loginCommand.Parsed() {
 		login()
 		return
 	}
